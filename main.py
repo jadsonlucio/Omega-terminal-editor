1da5f30c07af743695fff79ba9de5f6661fc427d
Jadsonlucio <jadsonaluno@hotmail.com>
2018-11-21 10:23:08 -0300

    add endwin to program exceptions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import curses
-from pyterminal.editor import Editor
+from omega.editor import Editor
+from omega.exceptions.exceptions import traceback
 
 if __name__ == "__main__":
     args = sys.argv
@@ -8,7 +9,10 @@ if __name__ == "__main__":
     if len(args) == 2:
         file_path = args[1]
 
-    print(file_path)
     editor = Editor()
     editor._create_screen(file_path = file_path)
-    editor.run()
+    try:
+        editor.run()
+    except:
+        curses.endwin()
+        print(traceback.format_exc())
